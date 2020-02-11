e0d9f2fe733b91fa135e46255572d60a97ede8d4
Максимилиан Мамиляев <msmamilyaev@edu.hse.ru>
2020-02-12 01:10:32 +0300

    1st Final Version

--- parser.py
+++ parser.py
@@ -23,7 +23,7 @@ vat = 20
 for node in root.findall('shop/offers/offer'):
     offer_id = node.get('id')
     name = node.find('name').text
-    shop_price = node.find('price').text
+    shop_price = float(node.find('price').text)
     pack_type = 'Поштучно'
     for item in node.iterfind('param[@name="Фасовка"]'):
         if item.text in weight:
@@ -34,22 +34,23 @@ for node in root.findall('shop/offers/offer'):
         pack_weight = float(item.text)
     for retailer in node.iterfind('outlets/outlet'):
         retailer_id = retailer.get("id")
-        retailer_stock = retailer.get("instock")
+        retailer_stock = int(retailer.get("instock"))
         retailer_price = float(retailer.find('price').text)
-        if (pack_type in ['Весовой', 'Фасованный'] or
-                abs(retailer_price - shop_price) >= max(retailer_price, shop_price) * 0.3):
-#здесь очень странный момент, очевидно(как мне кажется), что наценка не может быть 30%
-#однако, есть товары, в которых тип фасовки не указан, однако они продаются на развес,
-#напрмер, Азу или же какая-то разделка, к сожалению, мне не удалось найти схожести в их параметрах
-#поэтому я говорю, что если цена отличается более чем на 30%(беря в рассчёт, что максимальная порция
-# не превышает 700 грамм), значит, это цена уже за киллограмм и я также сразу меняю тип фасовки
-            retailer_price_kilo = retailer_price
+        if ((pack_type in ['Весовой', 'Фасованный'] or
+                abs(retailer_price - shop_price) >= max(retailer_price, shop_price) * 0.3)) and (retailer_price != 0):
+    #здесь очень странный момент, очевидно(как мне кажется), что наценка не может быть 30%
+    #однако, есть товары, в которых тип фасовки не указан, однако они продаются на развес,
+    #напрмер, Азу или же какая-то разделка, к сожалению, мне не удалось найти схожести в их параметрах
+    #поэтому я говорю, что если цена отличается более чем на 30%(беря в рассчёт, что максимальная порция
+    # не превышает 700 грамм), значит, это цена уже за киллограмм и я также сразу меняю тип фасовки
+            retailer_price_kilo = retailer_price / pack_weight
             if pack_type == 'Поштучно':
                 pack_type = 'Весовой'
         else:
             retailer_price_kilo = 0
-        dataFrame = dataFrame.append(pd.Series([offer_id, name, items_amount,
-                                                pack_type, vat, shop_price,
+        dataFrame = dataFrame.append(pd.Series([
+                                                offer_id, name, items_amount,
+                                                pack_type, vat, retailer_price,
                                                 retailer_price_kilo,
                                                 retailer_price,
                                                 retailer_stock, date_change,
